531f760d6ddea66e583c3b1be12011b78c2f5fa0
slezkinis <slezkin.is@mail.ru>
2023-04-12 18:56:12 +0300

    Add null for Place

--- places/models.py
+++ places/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 
 class Place(models.Model):
-    lat = models.FloatField(verbose_name='Геог. широта')
-    lon = models.FloatField(verbose_name='Геог. долгота')
+    lat = models.FloatField(verbose_name='Геог. широта', null=True)
+    lon = models.FloatField(verbose_name='Геог. долгота', null=True)
     adress = models.CharField('Адрес места', max_length=100)
     data = models.DateField('Дата запроса к геокодеру')
 
