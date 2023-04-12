da5233e0b862570d1fb6db1937a07fbb7fb98583
slezkinis <slezkin.is@mail.ru>
2023-04-12 19:00:04 +0300

    Add except for yandex_geo

--- restaurateur/views.py
+++ restaurateur/views.py
@@ -96,24 +96,6 @@ def view_restaurants(request):
     })
 
 
-def fetch_coordinates(apikey, address):
-    base_url = "https://geocode-maps.yandex.ru/1.x"
-    response = requests.get(base_url, params={
-        "geocode": address,
-        "apikey": apikey,
-        "format": "json",
-    })
-    response.raise_for_status()
-    found_places = response.json()['response']['GeoObjectCollection']['featureMember']
-
-    if not found_places:
-        return None
-
-    most_relevant = found_places[0]
-    lon, lat = most_relevant['GeoObject']['Point']['pos'].split(" ")
-    return lon, lat
-
-
 def get_distance(restaurant, order_coords):
     place = Place.objects.get(adress=restaurant.address)
     restaurant_coords = (place.lon, place.lat)
@@ -131,7 +113,11 @@ def view_orders(request):
     restaurants = []
     error = False
     for order in Order.objects.filter(executive_restaurant__isnull=True).calcurate_order_cost():
-        order_coords = fetch_coordinates(YANDEX_APIKEY, order.address)
+        try:
+            order_coords = fetch_coordinates(YANDEX_APIKEY, order.address)
+        except:
+            error = True
+            break
         for restaurant in Restaurant.objects.all():
             restaurant_menu = [product.product for product in restaurant.menu_items.all()]
             for order_product in order.products.all():
@@ -140,14 +126,15 @@ def view_orders(request):
             else:
                 restaurants.append(restaurant)
         restaurants_with_distances = []
-        for restaurant in restaurants:
-            restaurant_distance = get_distance(restaurant, order_coords)
-            if not restaurant_distance:
-                error = True
-                break
-            restaurants_with_distances.append(
-                (restaurant.name, restaurant_distance)
-            )
+        if not error:
+            for restaurant in restaurants:
+                restaurant_distance = get_distance(restaurant, order_coords)
+                if not restaurant_distance:
+                    error = True
+                    break
+                restaurants_with_distances.append(
+                    (restaurant.name, restaurant_distance)
+                )
         restaurants_with_distances = sorted(restaurants_with_distances, key=lambda restaurant: restaurant[1])
         orders.append(
             {
