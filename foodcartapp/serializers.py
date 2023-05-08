af8b8d3d4372ae164fb22fd92d61bf44515d098d
slezkinis <slezkin.is@mail.ru>
2023-05-08 10:50:07 +0300

    Use phonenumber_serializer

--- foodcartapp/serializers.py
+++ foodcartapp/serializers.py
@@ -1,8 +1,6 @@
-from rest_framework.serializers import ListField 
 from rest_framework.serializers import ModelSerializer
-from rest_framework.serializers import CharField
 from rest_framework.serializers import ValidationError
-import phonenumbers
+from phonenumber_field.serializerfields import PhoneNumberField
 
 from .models import Product, Order, OrderElements
 
@@ -27,6 +25,7 @@ class OrderElementsSerializer(ModelSerializer):
 
 class OrderSerializer(ModelSerializer):
     products = OrderElementsSerializer(many=True, allow_empty=False)
+    phonenumber = PhoneNumberField()
 
     def create(self, request):
         return Order.objects.create(
@@ -35,17 +34,6 @@ class OrderSerializer(ModelSerializer):
             lastname=request.data['lastname'],
             phonenumber=request.data['phonenumber'],
         )
-
-    
-    def validate_phonenumber(self, value):
-        if not isinstance(value, str):
-            raise ValidationError('phonenumber must be a string!')
-        if not value:
-            raise ValidationError("this field can't be empty")
-        parsed_number = phonenumbers.parse(value, 'RU')
-        if not phonenumbers.is_valid_number(parsed_number):
-            raise ValidationError('phonenumber is not valid')
-        return value
     
     class Meta:
         model = Order
